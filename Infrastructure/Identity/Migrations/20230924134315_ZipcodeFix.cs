ba8a923a49b3eeff50457389e5dfff81551e32be
KarolinaSzarek1284 <karolinaszarek@wp.pl>
2023-09-26 17:32:02 +0200

    End of section 19

---
+++ Infrastructure/Identity/Migrations/20230924134315_ZipcodeFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class ZipcodeFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ZipCode",
+                table: "Address",
+                newName: "Zipcode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Zipcode",
+                table: "Address",
+                newName: "ZipCode");
+        }
+    }
+}
